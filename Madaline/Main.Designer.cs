1d2856b17c5a06cbf8648e894b04348161043e3a
gerson <gersonmore123@gmail.com>
2021-05-01 11:37:15 -0500

    Agregado las referencias

--- Madaline/Main.Designer.cs
+++ Madaline/Main.Designer.cs
@@ -29,13 +29,314 @@ namespace Madaline
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.PnPresentacion = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PnContenido = new System.Windows.Forms.Panel();
+            this.NbIteracion = new System.Windows.Forms.NumericUpDown();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pnMenu.SuspendLayout();
+            this.PnPresentacion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NbIteracion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.Color.Black;
+            this.pnMenu.Controls.Add(this.label5);
+            this.pnMenu.Controls.Add(this.panel6);
+            this.pnMenu.Controls.Add(this.textBox1);
+            this.pnMenu.Controls.Add(this.numericUpDown2);
+            this.pnMenu.Controls.Add(this.panel5);
+            this.pnMenu.Controls.Add(this.label4);
+            this.pnMenu.Controls.Add(this.numericUpDown1);
+            this.pnMenu.Controls.Add(this.panel3);
+            this.pnMenu.Controls.Add(this.label3);
+            this.pnMenu.Controls.Add(this.NbIteracion);
+            this.pnMenu.Controls.Add(this.panel4);
+            this.pnMenu.Controls.Add(this.label6);
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMenu.Location = new System.Drawing.Point(0, 113);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(219, 548);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // PnPresentacion
+            // 
+            this.PnPresentacion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(3)))), ((int)(((byte)(69)))));
+            this.PnPresentacion.Controls.Add(this.label2);
+            this.PnPresentacion.Controls.Add(this.label1);
+            this.PnPresentacion.Controls.Add(this.panel2);
+            this.PnPresentacion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnPresentacion.Location = new System.Drawing.Point(0, 0);
+            this.PnPresentacion.Name = "PnPresentacion";
+            this.PnPresentacion.Size = new System.Drawing.Size(984, 113);
+            this.PnPresentacion.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(136, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Red neuronal Madaline";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(136, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gerson Moreno Castillo";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::Madaline.Properties.Resources._5897fbe2f421bb49d85633c45f2720b4;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(130, 113);
+            this.panel2.TabIndex = 0;
+            // 
+            // PnContenido
+            // 
+            this.PnContenido.BackColor = System.Drawing.Color.White;
+            this.PnContenido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnContenido.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.PnContenido.Location = new System.Drawing.Point(0, 0);
+            this.PnContenido.Name = "PnContenido";
+            this.PnContenido.Size = new System.Drawing.Size(984, 661);
+            this.PnContenido.TabIndex = 2;
+            // 
+            // NbIteracion
+            // 
+            this.NbIteracion.BackColor = System.Drawing.Color.Black;
+            this.NbIteracion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NbIteracion.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NbIteracion.ForeColor = System.Drawing.Color.White;
+            this.NbIteracion.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.NbIteracion.Location = new System.Drawing.Point(15, 44);
+            this.NbIteracion.Maximum = new decimal(new int[] {
+            -727379968,
+            232,
+            0,
+            0});
+            this.NbIteracion.Name = "NbIteracion";
+            this.NbIteracion.Size = new System.Drawing.Size(163, 18);
+            this.NbIteracion.TabIndex = 14;
+            this.NbIteracion.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panel4.Location = new System.Drawing.Point(15, 68);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(163, 1);
+            this.panel4.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label6.Location = new System.Drawing.Point(12, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Número de iteraciones";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDown1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.White;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 116);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -727379968,
+            232,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(163, 18);
+            this.numericUpDown1.TabIndex = 17;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panel3.Location = new System.Drawing.Point(15, 140);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(163, 1);
+            this.panel3.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Error maximo";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDown2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.ForeColor = System.Drawing.Color.White;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(15, 191);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            -727379968,
+            232,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(163, 18);
+            this.numericUpDown2.TabIndex = 20;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panel5.Location = new System.Drawing.Point(15, 215);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(163, 1);
+            this.panel5.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label4.Location = new System.Drawing.Point(12, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 15);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Rata de aprendizaje";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(15, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 15);
+            this.textBox1.TabIndex = 21;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panel6.Location = new System.Drawing.Point(16, 283);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(163, 1);
+            this.panel6.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label5.Location = new System.Drawing.Point(12, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 15);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Capas intermedias";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.pnMenu);
+            this.Controls.Add(this.PnPresentacion);
+            this.Controls.Add(this.PnContenido);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Opacity = 0.95D;
+            this.Text = "Madaline";
+            this.pnMenu.ResumeLayout(false);
+            this.pnMenu.PerformLayout();
+            this.PnPresentacion.ResumeLayout(false);
+            this.PnPresentacion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NbIteracion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Panel PnContenido;
+        private System.Windows.Forms.Panel PnPresentacion;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NbIteracion;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
